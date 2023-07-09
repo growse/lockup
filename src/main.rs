32aa6ab9d79c022cb825d4c382a35468a7d41b9f
Andrew Rowson <github@growse.com>
2023-07-09 21:51:51 +0100

    SQLX

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,63 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fairing::AdHoc;
+use rocket::futures::TryStreamExt;
+use rocket::serde::{json::Json, Deserialize, Serialize, Serializer};
+use rocket::{fairing, Build, Rocket};
+use rocket_db_pools::{sqlx, Connection, Database};
 use std::error::Error;
-use std::path::Path;
 use std::result;
-use std::sync::Mutex;
-use std::time::Instant;
+use std::time::SystemTime;
 
-use rocket::http::ext::IntoCollection;
-use rocket::{Build, Ignite, Rocket, State};
-use rusqlite::Connection;
-use rusqlite_migration::{Migrations, M};
-use url::Url;
+type Result<T, E = rocket::response::Debug<sqlx::Error>> = result::Result<T, E>;
 
-struct Config {
-    db: Mutex<Connection>,
-}
-
-type Result<T> = result::Result<T, Box<dyn Error>>;
-
-#[derive(Responder)]
-#[response(status = 200, content_type = "json")]
-struct SuccessJson(&'static str);
+#[derive(Database)]
+#[database("sqlite_things")]
+struct ThingsDb(sqlx::SqlitePool);
 
 #[get("/")]
-fn index() -> SuccessJson {
-    SuccessJson("Hello, world!")
+fn index() -> Json<String> {
+    Json("Hi there".to_string())
 }
 
 #[get("/things")]
-fn list() -> SuccessJson {
-    SuccessJson("hi")
+async fn list(mut db: Connection<ThingsDb>) -> Result<Json<Vec<i64>>> {
+    let ids = sqlx::query!("SELECT id FROM things")
+        .fetch(&mut *db)
+        .map_ok(|record| record.id)
+        .try_collect::<Vec<_>>()
+        .await?;
+
+    Ok(Json(ids))
 }
 
 #[launch]
 fn rocket() -> _ {
-    let db = init_db(Path::new("lockup.sqlite")).expect("Bzzt");
-    let config = Config { db: Mutex::new(db) };
     rocket::build()
+        .attach(ThingsDb::init())
+        .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
         .mount("/", routes![index, list])
-        .manage(config)
 }
 
-fn init_db(path: &Path) -> Result<Connection> {
-    // let migrations = Migrations::new(vec![M::up(
-    //     "CREATE TABLE things(id int not null, url text not null, added text not null);",
-    // )]);
-    let mut conn = Connection::open(path)?;
-    // conn.pragma_update(None, "journal_mode", &"WAL")?;
-    // migrations.to_latest(&mut conn)?;
-    Ok(conn)
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+    match ThingsDb::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!("db/sqlx/migrations").run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Failed to initialize SQLx database: {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Thing {
-    id: i32,
-    url: Url,
-    added: Instant,
+    id: i64,
+    url: String,
+    added: SystemTime,
     tags: Vec<String>,
 }
