71ce1a0096ec42ece164a3d976f2d91b08981dbc
Andrew Rowson <github@growse.com>
2023-08-05 16:58:06 +0100

    Types as enums

--- migrations/20230714100427_initial.sql
+++ migrations/20230714100427_initial.sql
@@ -1,11 +1,27 @@
--- Add migration script here
 create table things
 (
     id    integer primary key autoincrement,
     url   text                                                                  not null,
-    added integer                                                               not null default CURRENT_TIMESTAMP,
-    tags  text                                                                  not null default '',
+    added datetime                                                              not null default CURRENT_TIMESTAMP,
     type  text check (type in ('article', 'youtube', 'podcast', 'rss', 'file')) not null default 'article'
 );
 
 CREATE UNIQUE INDEX idx_things_url ON things (url);
+
+CREATE TABLE tags
+(
+    id  integer primary key autoincrement,
+    tag text not null
+);
+
+CREATE UNIQUE INDEX idx_tags_tag ON tags (tag);
+
+CREATE TABLE thing_tags
+(
+    thing integer not null,
+    tag   integer not null,
+    foreign key (thing) references things (id),
+    foreign key (tag) references tags (id)
+);
+
+CREATE UNIQUE INDEX idx_thing_tags ON thing_tags (thing, tag);
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #[macro_use]
 extern crate rocket;
 
+use crate::AddThingError::DatabaseError;
+use chrono::{NaiveDateTime};
 use rocket::fairing::AdHoc;
 use rocket::futures::TryStreamExt;
+use rocket::response::status::Created;
+use rocket::response::Responder;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::{fairing, Build, Rocket};
 use rocket_db_pools::{sqlx, Connection, Database};
-use rocket::response::status::{Created};
 use std::result;
-use std::time::SystemTime;
-use rocket::response::Responder;
-use crate::AddThingError::DatabaseError;
 
 type Result<T, E = rocket::response::Debug<sqlx::Error>> = result::Result<T, E>;
 
@@ -18,22 +18,46 @@ type Result<T, E = rocket::response::Debug<sqlx::Error>> = result::Result<T, E>;
 #[database("thingsdb")]
 struct ThingsDb(sqlx::SqlitePool);
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Thing {
+    pub id: i64,
+    pub url: String,
+    pub added: NaiveDateTime,
+    // pub tags: Vec<Tag>,
+     type_: Type,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Tag {
+    pub id: i64,
+    pub tag: String,
+}
+
+#[derive(Debug, Clone,sqlx::Type,Serialize,Deserialize)]
+#[sqlx(rename_all = "lowercase")]
+enum Type {
+    Article,
+    Youtube,
+    Podcast,
+    RSS,
+    File,
+ }
+
 #[get("/")]
 fn index() -> Json<String> {
     Json("Hi there".to_string())
 }
 
 #[get("/things")]
-async fn list(mut db: Connection<ThingsDb>) -> Result<Json<Vec<i64>>> {
-    let ids = sqlx::query!("SELECT id,url FROM things")
+async fn list(mut db: Connection<ThingsDb>) -> Result<Json<Vec<Thing>>> {
+    let things = sqlx::query_as!(Thing, r#"SELECT things.id, url, added,type as "type_: Type" FROM things "#)
         .fetch(&mut *db)
-        .map_ok(|record| record.id)
         .try_collect::<Vec<_>>()
-        .await?
-        .into_iter().flatten()
-        .collect();
+        .await?;
 
-    Ok(Json(ids))
+    Ok(Json(things))
 }
 
 #[derive(Responder)]
@@ -45,15 +69,22 @@ enum AddThingError {
 }
 
 #[post("/things", data = "<url>")]
-async fn add_thing(mut db: Connection<ThingsDb>, url: &str) -> result::Result<Created<&str>, AddThingError> {
+async fn add_thing(
+    mut db: Connection<ThingsDb>,
+    url: &str,
+) -> result::Result<Created<&str>, AddThingError> {
     let parsed_url = url::Url::parse(url);
     if parsed_url.is_ok() {
-        sqlx::query!("INSERT INTO things (url) values (?)",url)
+        sqlx::query!("INSERT INTO things (url) values (?)", url)
             .execute(&mut *db)
-            .await.map_err(|e| DatabaseError(format!("database error: {}",e).to_string()))?;
+            .await
+            .map_err(|e| DatabaseError(format!("database error: {}", e).to_string()))?;
         Ok(Created::new("/things").body("Added".as_ref()))
     } else {
-        Err(AddThingError::NotAValidURL( format!("Not a URL: {}",url.to_string())))
+        Err(AddThingError::NotAValidURL(format!(
+            "Not a URL: {}",
+            url.to_string()
+        )))
     }
 }
 
@@ -76,13 +107,4 @@ async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
         },
         None => Err(rocket),
     }
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(crate = "rocket::serde")]
-struct Thing {
-    id: i64,
-    url: String,
-    added: SystemTime,
-    tags: Vec<String>,
 }
